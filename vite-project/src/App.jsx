847bbc9fc635ba2cfc35b912e14c8f8864e7834b
Warren <warrendsteinacker@gmail.com>
2025-12-17 15:31:26 -0500

    think I created proper use effect

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -8,10 +8,16 @@ function App() {
 
   useEffect(() => {
     try{
-
-    }
+        const d = JSON.parse(localStorage.getItem('item'))
+        if (d){
+          setData(d)
+        }
+        else{
+          setData([])
+        }
+      }
     catch(error){
-
+        console.error(error.message)
     }
 
   }, []
