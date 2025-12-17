011a13e0fa239a4c049dce3b59ade2bb209527a0
Warren <warrendsteinacker@gmail.com>
2025-12-17 16:25:59 -0500

    working on routes

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import {BrowserRouter, Route, Routes, useNavigate} from 'react-router-dom'
 import { Home } from './componets/homepage'
 import { Ppage } from './componets/postpage'
 import { Epage } from './componets/editpage'
@@ -54,9 +54,10 @@ const epost = () => {
     <>
     <BrowserRouter>
     <Routes>
-      <Route path="/" element/>
-      <Route/>
-      <Route/>
+      <Route path="/" element={<Home />}/>
+      <Route path="/ppage" element={<Ppage />}/>
+      <Route path="/epage" element={<Epage />}/>
+      <Route path="*" element={<h1>Page not found</h1>}/>
     </Routes>
     </BrowserRouter>
     </>
