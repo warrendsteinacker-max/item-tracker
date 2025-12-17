ae3dd2ad833a3397f9051a88c84ee078302a8bb4
Warren <warrendsteinacker@gmail.com>
2025-12-17 16:41:45 -0500

    think I got my context set up right

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -7,48 +7,6 @@ import { Epage } from './componets/editpage'
 
 
 function App() {
-  const [data, setData] = useState([])
-  const [post, setPost] = useState([])
-  const [poste, setPoste] = useState([])
-
-  useEffect(() => {
-    try{
-        const d = JSON.parse(localStorage.getItem('item'))
-        if (d){
-          setData(d)
-        }
-        else{
-          setData([])
-        }
-      }
-    catch(error){
-        console.error(error.message)
-    }
-
-  }, []
-)
-
-//all good
-
-const posting = (newpost) => {
-    try{
-      const allp = [...data, newpost]
-      setData(allp)
-    }
-    catch(error){
-      console.error(error.message)
-    }
-}
-
-const del = () => {
-
-}
-
-const epost = () => {
-
-}
-
-
 
   return (
     <>
--- vite-project/src/context.jsx
+++ vite-project/src/context.jsx
@@ -1,4 +1,4 @@
-import { Children, createContext } from "react";
+import { createContext } from "react";
 import { useState, useEffect } from 'react';
 
 
@@ -8,12 +8,53 @@ export const DataContext = createContext({
 
 
 
-export const DataProvider = (Children) => {
+export const DataProvider = ({children}) => {
 
+      const [data, setData] = useState([])
+      const [post, setPost] = useState([])
+      const [poste, setPoste] = useState([])
+    
+      useEffect(() => {
+        try{
+            const d = JSON.parse(localStorage.getItem('item'))
+            if (d){
+              setData(d)
+            }
+            else{
+              setData([])
+            }
+          }
+        catch(error){
+            console.error(error.message)
+        }
+    
+      }, []
+    )
+    
+    //all good
+    
+    const posting = (newpost) => {
+        try{
+          const allp = [...data, newpost]
+          setData(allp)
+        }
+        catch(error){
+          console.error(error.message)
+        }
+    }
+    
+    const del = () => {
+    
+    }
+    
+    const epost = () => {
+    
+    }
+    
 
     return(
-        <DataContext.Provider>
-            {Children}
+        <DataContext.Provider value={{}}>
+            {children}
         </DataContext.Provider>
     )
 }
