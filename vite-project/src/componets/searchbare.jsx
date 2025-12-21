652ff5b2a79b096dac5b4e83bedc137426a54a79
Warren <warrendsteinacker@gmail.com>
2025-12-20 19:18:49 -0500

    testing more with css

--- vite-project/src/componets/searchbare.jsx
+++ vite-project/src/componets/searchbare.jsx
@@ -11,7 +11,7 @@ export const Search = () => {
         placeholder="Search items..."
         value={search}
         onChange={(e) => setSearch(e.target.value)}
-        style={{ width: '100%', padding: '10px', fontSize: '16px' }}
+        style={{ width: '100%', padding: '10px', fontSize: '16px', borderRadius: '10px' }}
       />
     </div>
   );
