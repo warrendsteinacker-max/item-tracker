cda1be4e80cc9e4a17f231a5086310ecaf29eea9
Warren <warrendsteinacker@gmail.com>
2025-12-26 13:40:27 -0500

    added ref in input feild for foward ref

--- vite-project/src/componets/ref.jsx
+++ vite-project/src/componets/ref.jsx
@@ -6,16 +6,21 @@ const refC = memo(forwardRef((props, ref) => {
  
     return(
         <div>
-            <></>
+            <h4>item is {props.item}</h4>
+            <button onClick={props.inc}>+</button>
+            <button onClick={props.dec}>-</button>
+            <button onClick={props.reset}>reset</button>
+            <input type="text" placeholder={props.item} ref={ref} />
+            <input type="text" placeholder="input item" onChange={}/>
         </div>
     )
 }))
 
 export const Ref = () => {
 
-    const II = {count: AA, item: 'apple', priceofc: 5}
-
     const AA = useCallback(() => {for(let i=0; i<1000000000; i++){if(i===1000000000){return i;}}} , [II.count]);
+
+    const II = {count: AA, item: 'apple', priceofc: 5}
 
     const [state, dispatch] = useReducer(reducer, II);
         reducer = (state, action) => {
@@ -24,8 +29,14 @@ export const Ref = () => {
                     return {...state, count: state.count + 1};
                 case 'dec':
                     return {...state, count: state.count -1};
-                case
+                case 'reset':
+                    return {...state, count: 0};
+                default:
+                    return state;
+            }
             
-            <refC />
-    )
+            return(
+                <refC inc={() => dispatch({type: 'inc'})} dec={() => dispatch({type: 'dec'})} reset={() => dispatch({type: 'reset'})} />
+            )
+
 }
