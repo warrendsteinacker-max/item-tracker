ec104d9dbb8a3e6975d45c5e0b2d95d99989797e
Warren <warrendsteinacker@gmail.com>
2025-12-26 14:19:23 -0500

    fixed it

--- vite-project/src/componets/ref.jsx
+++ vite-project/src/componets/ref.jsx
@@ -1,51 +1,67 @@
 import { forwardRef, useReducer, useMemo, useCallback, memo, useRef } from "react";
 
+// 1. Move reducer outside and define properly
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'inc':
+            return { ...state, count: state.count + 1 };
+        case 'dec':
+            return { ...state, count: state.count - 1 };
+        case 'reset':
+            return { ...state, count: 0 };
+        default:
+            return state;
+    }
+};
 
-
-const refC = memo(forwardRef((props, ref) => {
- 
-    return(
+// 2. Component name must be Uppercase
+const RefC = memo(forwardRef((props, ref) => {
+    return (
         <div>
             <h4>item is {props.item}</h4>
             <h4>count is {props.count}</h4>
             <button onClick={props.inc}>+</button>
             <button onClick={props.dec}>-</button>
             <button onClick={props.reset}>reset</button>
+            <br />
             <input type="text" placeholder={props.item} ref={ref} />
-            <input type="text" placeholder="input item" onChange={props.placeHchange}/>
+            <input type="text" placeholder="input item" onChange={props.placeHchange} />
         </div>
-    )
+    );
 }));
 
 export const Ref = () => {
-
     const reff = useRef();
 
-    placeHchange = (e) => {
-        reff.current.placeholder = e.target.value;
-    }
+    // 3. Initial state object
+    const initialState = { count: 0, item: 'apple', priceofc: 5 };
+    const [state, dispatch] = useReducer(reducer, initialState);
 
-    const AA = useCallback(() => {for(let i=0; i<1000000000; i++){if(i===1000000000){return i;}}} , [II.count]);
+    // 4. Declare function with const
+    const handlePlaceHchange = (e) => {
+        if (reff.current) {
+            reff.current.placeholder = e.target.value;
+        }
+    };
 
-    const AAA = useMemo(() => AA(), [AA]);
+    // 5. Optimization (Note: a loop of 1 billion will freeze the browser for a moment)
+    const expensiveValue = useMemo(() => {
+        let result = 0;
+        for (let i = 0; i < 1000000; i++) { // Reduced for performance
+            if (i === 999999) result = i;
+        }
+        return result;
+    }, []); 
 
-    const II = {count: AAA, item: 'apple', priceofc: 5}
-
-    const [state, dispatch] = useReducer(reducer, II);
-        reducer = (state, action) => {
-            switch(action.type){
-                case 'inc':
-                    return {...state, count: state.count + 1};
-                case 'dec':
-                    return {...state, count: state.count -1};
-                case 'reset':
-                    return {...state, count: 0};
-                default:
-                    return state;
-            }
-            
-            return(
-                <refC ref={reff} inc={() => dispatch({type: 'inc'})} dec={() => dispatch({type: 'dec'})} reset={() => dispatch({type: 'reset'}), II={II}} />
-            )
-
-}}
+    return (
+        <RefC 
+            ref={reff} 
+            item={state.item}
+            count={state.count}
+            inc={() => dispatch({ type: 'inc' })} 
+            dec={() => dispatch({ type: 'dec' })} 
+            reset={() => dispatch({ type: 'reset' })} 
+            placeHchange={handlePlaceHchange}
+        />
+    );
+};
