896b4fddd47c5a0d8cf1c461cebc60beabaae372
Warren <warrendsteinacker@gmail.com>
2025-12-26 13:26:29 -0500

    makeing a expensive calculation

--- vite-project/src/componets/ref.jsx
+++ vite-project/src/componets/ref.jsx
@@ -3,11 +3,7 @@ import { forwardRef, useReducer, useMemo, useCallback, memo } from "react";
 
 
 const refC = memo(forwardRef((props, ref) => {
-
-const [state, dispatch] = useReducer(reducer, initialState); 
-
-
-
+ 
     return(
         <div>
             <></>
@@ -17,9 +13,19 @@ const [state, dispatch] = useReducer(reducer, initialState);
 
 export const Ref = () => {
 
+    const II = {count: AA, item: 'apple', priceofc: 5}
 
-    
-    return(
-        <refC />
+    const AA = useCallback(() => {for(let i=0; i<1000000000; i++){if(i===1000000000){return i;}}} , [II.count]);
+
+    const [state, dispatch] = useReducer(reducer, II);
+        reducer = (state, action) => {
+            switch(action.type){
+                case 'inc':
+                    return {...state, count: state.count + 1};
+                case 'dec':
+                    return {...state, count: state.count -1};
+                case
+            
+            <refC />
     )
 }
