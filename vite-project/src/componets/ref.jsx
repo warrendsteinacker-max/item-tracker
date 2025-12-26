fa634e915bed6765be9230eb3b522187166e26c3
Warren <warrendsteinacker@gmail.com>
2025-12-26 13:11:33 -0500

    got the struct for this page

--- vite-project/src/componets/ref.jsx
+++ vite-project/src/componets/ref.jsx
@@ -1,9 +1,25 @@
 import { forwardRef, useReducer, useMemo, useCallback, memo } from "react";
 
+
+
 const refC = memo(forwardRef((props, ref) => {
+
+const [state, dispatch] = useReducer(reducer, initialState); 
+
+
+
     return(
         <div>
             <></>
         </div>
     )
 }))
+
+export const Ref = () => {
+
+
+    
+    return(
+        <refC />
+    )
+}
