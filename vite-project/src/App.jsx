d8b8dcee1ea379cc483b2368120a4907baa837ec
Warren <warrendsteinacker@gmail.com>
2025-12-28 14:46:43 -0500

    cleaning up project to publish

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,9 +4,7 @@ import { Home } from './componets/homepage'
 import { Ppage } from './componets/postpage'
 import { Epage } from './componets/editpage'
 import { DataProvider } from './context'
-import { PC } from './componets/practice'
-import { Box } from './componets/box'
-import { C } from './componets/C'
+
 
 ///got it
 
@@ -19,9 +17,6 @@ function App() {
     <DataProvider>
     <BrowserRouter>
     <Routes>
-      <Route path="/ps" element={<C />}/>
-      <Route path="/pp/:id" element={<Box />}></Route>
-      <Route path="/p" element={<PC Dd={[{name: 'e', age: 5, id: 55}, {name: 'd', age: 4, id: 56}, {name: 'c', age: 3, id: 57}, {name: 'b', age: 2, id: 58}, {name: 'a', age: 1, id: 59}]} />}/>
       <Route path="/" element={<Home />}/>
       <Route path="/ppage" element={<Ppage />}/>
       <Route path="/epage" element={<Epage />}/>
